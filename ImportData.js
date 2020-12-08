d204db32e42eb566b14091221b4ca5fe5c4a570c
Manoj Babu Porur Ragu <manojbabu.ragu@genesys.com>
2020-12-08 12:18:49 +0530

    adding promises for multiprocess at same time in import

--- ImportData.js
+++ ImportData.js
@@ -3,46 +3,58 @@ var fs = require('fs');
 AWS.config.update({
     region: "us-east-1"
 });
-
-function ImportData(file)
-{
-    var docClient = new AWS.DynamoDB.DocumentClient();
-    console.log(`Importing the File ${file}`);
-    var allfeedback = JSON.parse(fs.readFileSync(file, 'utf8'));
-        allfeedback.forEach(function(dataItem) {
-        var params = {
-            TableName: "STA_FeedBack_Test1",
-            Item: dataItem
-        };
-        docClient.put(params, function(err, data) {
-           if (err) {
-               console.error(`Unable to add feeds in file ${file}`, dataItem.ID, ". Error JSON:", JSON.stringify(err, null, 2));
-           } else {
-               console.log(`file ${file} PutItem succeeded: ${dataItem.ID}`);
-           }
-        });
+ 
+function ImportDir(){
+    var dir = './dataFiles';
+    var files = [];
+    fs.stat(dir, async (err, stat) => {
+        if(err == null) {
+            console.log("Importing sample data into DynamoDB. Please wait.");
+            console.log('FeedBack Data folder exist');
+            files = fs.readdirSync(dir);
+            await Promise.all(files.map(async (file) => { await ImportFile(dir,file); }));
+        } else if(err.code === 'ENOENT') {
+            console.log('FeedBack Data File doesn\'t exists');
+            process.exit(1);
+        } else {
+            console.log('Some other error: ', err.code);
+            process.exit(1);
+        }
     });
 }
-var dir = './dataFiles';
-fs.stat(dir, function(err, stat) {
-    if(err == null) {
-        console.log("Importing sample data into DynamoDB. Please wait.");
-        console.log('FeedBack Data folder exist');
-        fs.readdirSync(dir).forEach(file => {
-            if(file.startsWith('feedBack_'))
-            {
-                ImportData(dir+'/'+file);
-            }
-          });
-        
-    } else if(err.code === 'ENOENT') {
-        console.log('FeedBack Data File doesn\'t exists');
-        process.exit(1);
-    } else {
-        console.log('Some other error: ', err.code);
-        process.exit(1);
+
+async function ImportFile(dir, file)
+{
+    if(file.startsWith('feedBack_'))
+    {
+        await ImportData(dir+'/'+file);
     }
-});
+}
+
+async function ImportData(file)
+{
+    console.log(`Importing the File ${file}`);
+    var allfeedback = JSON.parse(fs.readFileSync(file, 'utf8'));
+    await Promise.all(allfeedback.map(async (dataItem) => { await putData(file,dataItem); }));
+}
+
+async function putData(file, dataItem)
+{
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    var params = {
+        TableName: "STA_FeedBack_Test1",
+        Item: dataItem
+    };
+    await docClient.put(params, (err, data) => {
+       if (err) {
+           console.error(`Unable to add feeds in file ${file}`, dataItem.ID, ". Error JSON:", JSON.stringify(err, null, 2));
+       } else {
+           console.log(`File ${file} PutItem succeeded: ${JSON.stringify(dataItem.ID)}`);
+       }
+    });
+}
+
+ImportDir();
 
 
 

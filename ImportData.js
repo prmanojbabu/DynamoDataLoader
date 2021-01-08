1c514aed4edc4d3332dbb7e2ed65cbce1d42e344
Manoj Babu Porur Ragu <manojbabu.ragu@genesys.com>
2021-01-08 18:19:36 +0530

    saving changes

--- ImportData.js
+++ ImportData.js
@@ -1,12 +1,10 @@
 var AWS = require("aws-sdk");
 var fs = require('fs');
-require('dotenv').config();
-AWS.config.update({
-    region: process.env.region
-});
+
 
 function generateResultDir()
 {
+    console.log('Generating Result Insert');
     var dir = './ResultInsert';
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
@@ -17,8 +15,28 @@ function generateResultDir()
         fs.mkdirSync(dir);
     }
 }
-function ImportDir(){
-    var dir = './dataFiles';
+
+async function ImportDataSample(config)
+{
+    generateResultDir();
+    AWS.config.update({
+        region: config.Region
+    });
+    const createJobs = []
+    config.DBSampling.forEach(element => {
+        createJobs.push(
+            new Promise((resolve, reject) =>
+            {
+            const TableName = `${config.MainTableNamePrefix}_Main_${element.OrganizationCount}_${element.TotalRecordPerOrg}_${element.OrganizationCount*element.TotalRecordPerOrg}`;
+            var dir = `./SampleDataFiles/${TableName}`;
+            resolve(ImportDir(dir));
+            }));
+        });
+    await Promise.all(createJobs);
+}
+
+
+function ImportDir(dir){
     var files = [];
     fs.stat(dir, async (err, stat) => {
         if(err == null) {
@@ -38,22 +56,18 @@ function ImportDir(){
 
 async function ImportFile(dir, file)
 {
-    if(file.startsWith('feedBack_'))
-    {
-        await ImportData(dir+'/'+file);
-    }
+    await ImportData(dir+'/'+file);
 }
 
-async function ImportData(file)
+async function ImportData(MainTableName,MetaDataTable,File)
 {
     console.log(`Importing the File ${file}`);
-    var allfeedback = JSON.parse(fs.readFileSync(file, 'utf8'));
-    await Promise.all(allfeedback.map(async (dataItem) => { await putData(file,dataItem); }));
+    var allfeedback = JSON.parse(fs.readFileSync(File, 'utf8'));
+    await Promise.all(allfeedback.map(async (dataItem) => { await putData(dataItem, MainTableName, MetaDataTable); }));
 }
 
-async function putData(file, dataItem)
+async function putData(dataItem, MainTableName, MetaDataTable)
 {
-    var docClient = new AWS.DynamoDB.DocumentClient();
     var params = {
         TransactItems: [
             // {
@@ -63,7 +77,7 @@ async function putData(file, dataItem)
             //             'OrgId': dataItem.OrgId,
             //             'SortKey': 'SentimentFeedbackItemCount'
             //         },
-            //         TableName: process.env.dbName,
+            //         TableName: MetaDataTable,
             //         ExpressionAttributeValues: {
             //             ':MAX': 2000
             //         },
@@ -73,42 +87,43 @@ async function putData(file, dataItem)
             {
                 Put: {
                     Item: dataItem,
-                    TableName: process.env.dbName,
+                    TableName: MainTableName,
                     ConditionExpression: '(attribute_not_exists(OrgId) AND attribute_not_exists(SortKey)) AND (attribute_not_exists(OrgId) AND attribute_not_exists(ID))',
                     ReturnValuesOnConditionCheckFailure: "ALL_OLD",
                 }
-            },
-            {
-                Update: {
-                    Key: {
-                        'OrgId': dataItem.OrgId,
-                    },
-                    TableName: process.env.dbMetaDataName,
-                    UpdateExpression: 'ADD SentimentFeedbackItemCount :incr SET LastUpdateDate = :currentdate',
-                    ExpressionAttributeValues: {
-                        ':incr': 1,
-                        ':currentdate' : new Date().toUTCString()
-                    },
-                    ReturnValuesOnConditionCheckFailure: "NONE",
-                }
             }
+            // ,{
+            //     Update: {
+            //         Key: {
+            //             'OrgId': dataItem.OrgId,
+            //         },
+            //         TableName: MetaDataTable,
+            //         UpdateExpression: 'ADD SentimentFeedbackItemCount :incr SET LastUpdateDate = :currentdate',
+            //         ExpressionAttributeValues: {
+            //             ':incr': 1,
+            //             ':currentdate' : new Date().toUTCString()
+            //         },
+            //         ReturnValuesOnConditionCheckFailure: "NONE",
+            //     }
+            // }
                 
         ],
     ReturnConsumedCapacity: "INDEXES"
 };
-await docClient.transactWrite(params, (err, data) => {
-    if (err) {
-        console.error(`Unable to add feeds in file ${file}`, params, ". Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("File :" + file + " Success: " + params.TransactItems);
-        fs.appendFileSync(`./ResultInsert/Insert_Output_${params.TransactItems}.json`, JSON.stringify(params.TransactItems.Put, null, "\t") + ",\n", 'utf8');
-
-    }
-});
+await WriteTransaction(param);
 }
 
-generateResultDir();
-ImportDir();
+async function WriteTransaction(params)
+{
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    await docClient.transactWrite(params, (err, data) => {
+        if (err) {
+            console.error(`Unable to add feeds in file`, params, ". Error JSON:", JSON.stringify(err, null, 2));
+        }
+        if(data) {
+            fs.appendFileSync(`./ResultInsert/Insert_Output.json`, JSON.stringify(data, null, "\t") + ",\n", 'utf8');
+        }
+    });
+}
 
-
-
+module.exports = {ImportDataSample};
--- StartMusic.js
+++ StartMusic.js
@@ -1,8 +1,10 @@
 var DataGenerator = require('./DataGenerator');
 var CreateTable = require('./CreateTable');
 var DeleteTable = require('./DeleteTable');
+var ImportData = require('./ImportData');
 var config = require('./ReadConfig');
 
-//DataGenerator.GenerateDB(config);
-//CreateTable.CreateSampleTables(config);
-DeleteTable.DeleteSampleTables(config);
+// DataGenerator.GenerateDB(config);
+// CreateTable.CreateSampleTables(config);
+// ImportData.ImportDataSample(config);
+// DeleteTable.DeleteSampleTables(config);

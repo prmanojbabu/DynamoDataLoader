5fdd81ceee88e13ef6ac042deb5a22c479f62c8b
Shreya Chauhan <chauhanshreya25@gmail.com>
2021-01-21 15:01:44 +0530

    Test

--- DynamicDataGenerator.js
+++ DynamicDataGenerator.js
@@ -1,3 +1,5 @@
+var zlib = require("zlib");
+var sizeof = require('object-sizeof');
 var faker = require('faker');
 var _ = require('lodash');
 var prompt = require('prompt-sync')({sigint: true});
@@ -11,9 +13,9 @@ var initialSentiment = ["Positive", "Negative","Neutral"] ;
 var feedBackData = [];
 var OrgIdS = [];
 
-var RecordCount = prompt('Enter Total Record required to load(Default :20000):', 20000);
-var maxOrgCount = prompt('Enter Maximum records per Organization((Default :2000)):', 2000);
-var maxRecordPerFile = prompt('Enter Maximum records per File((Default :20000)):', 20000);
+// var RecordCount = prompt('Enter Total Record required to load(Default :20000):', 1);
+// var maxOrgCount = prompt('Enter Maximum records per Organization((Default :2000)):', 1);
+// var maxRecordPerFile = prompt('Enter Maximum records per File((Default :20000)):', 20000);
 function  validateInput() {
     if(!isNaN(RecordCount) && Number(RecordCount) >0)
     {
@@ -44,19 +46,13 @@ function  validateInput() {
     console.log('Total Records per Org: '+ maxOrgCount);
     console.log('Total Records per file: '+ maxRecordPerFile);
 }
-function generateData()
+function generateData(phraseSize=10)
 {
     var OrgId = faker.random.uuid();
-    for(i=0; i<RecordCount; i++)
-    {
-        if(i % maxOrgCount === 0)    
-        {
-            OrgId=faker.random.uuid();
-            OrgIdS.push(OrgId);    
-        }
+    
         var feedbackValue = _.sample(feedbackSentiment);
         var initialValue =  _.sample((_.filter(initialSentiment, (x)=> {return x!=feedbackValue;})));
-        var phrase = faker.random.words(faker.random.number({min: 3, max: 20}));
+        var phrase = faker.random.words(phraseSize);
         var item = {
                 "OrgId":  OrgId,
                 "Id": faker.random.uuid(),
@@ -70,8 +66,7 @@ function generateData()
                 "CreatedDate": faker.date.between('2000-01-01T00:00:00.000Z', '2020-12-01T00:00:00.000Z'),
                 "SortKey" : `SentimentFeedback|Lang_${langdetect.detectOne(phrase)}|StemmedPhrase_${stemmer_es.stem(phrase)}`
                 };
-    feedBackData.push(item);
-    }
+    return item;
 }
 function chunkArray(myArray, chunk_size){
     var chunks = [], i = 0, n = myArray.length;
@@ -98,9 +93,53 @@ function generateJSONFiles()
     chunksFeedback.forEach((x, index) => {fs.writeFileSync(`./dataFiles/feedBack_${index}.json`, JSON.stringify(x, null, "\t"), 'utf8');});
     chunksOrg.forEach((x, index) => {fs.writeFileSync(`./dataFiles/org_${index}.json`, JSON.stringify(x, null, "\t"), 'utf8');});
     console.log('Files Created Successfully');
+    sizeCalculate();
+    
+}
+
+function sizeCalculate(){
+    // var stats = fs.statSync(`./dataFiles/feedBack_0.json`);
+    // var fileSizeInBytes = stats.size;
+    // var fileSizeInKilobytes = fileSizeInBytes / (1024);
+    // console.log(`File Size: ${fileSizeInKilobytes} KB`);
+
+    const dataview = JSON.stringify((JSON.parse(fs.readFileSync(`./dataFiles/feedBack_0.json`,'utf8'))[0]));
+    console.log(`Data size: ${sizeof(dataview)/1024} KB`);
+    const compressedResponse =  zlib.gzipSync(dataview);   
+    console.log(`Compressed Data size: ${sizeof(compressedResponse)/1024} KB`);
 }
 
 
-validateInput();
-generateData();
-generateJSONFiles();
+// validateInput();
+// generateData();
+// generateJSONFiles();
+
+function test()
+{
+    var dir = './dataFiles';
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+    else
+    {
+        fs.rmdirSync(dir, { recursive: true });
+        fs.mkdirSync(dir);
+    }
+
+    for(var i=3; i<=20; i++)
+    {
+        const obj = [];
+        for(var k=0; k<=2000; k++)
+        {
+            const item = generateData(i);
+            obj.push(item);
+        }
+        const compressedResponse =  zlib.gzipSync(JSON.stringify(obj));
+        fs.writeFileSync(`./dataFiles/feedBack_${i}_NonCompressed`, JSON.stringify(obj), 'utf8');
+        fs.writeFileSync(`./dataFiles/feedBack_${i}_Compressed`, compressedResponse, 'binary');
+        var _NonCompressed = fs.statSync(`./dataFiles/feedBack_${i}_NonCompressed`).size/1024/1024;
+        var _Compressed = fs.statSync(`./dataFiles/feedBack_${i}_Compressed`).size/1024/1024;
+        console.log(`PhraseWordslength: ${i}\tDatasize: ${_NonCompressed}\tMB \tCompressedDatasize: ${_Compressed}\tMB\tPhraselength : ${obj[0].Phrase.length}\tPhrase: ${obj[0].Phrase}`);
+    }
+}
+test();
